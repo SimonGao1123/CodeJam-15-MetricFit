04fdd9f760a8e4b874e943b12922c6ded8e066c0
Simon Gao <simongao398@gmail.com>
2025-11-14 19:15:15 -0500

    initial commit

---
+++ healthHabitTracker/backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import loginRoutes from './routes/loginRoutes.js';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/", loginRoutes);
+
+app.listen(3000, () => console.log("Server has started"));
